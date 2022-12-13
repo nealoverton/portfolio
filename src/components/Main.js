73ab5d7c1cc536821b2dc0d0ceb7e784b2801195
nealoverton <nealjoverton@gmail.com>
2022-12-13 17:29:02 +0000

    display main info space

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import '../styles/styles.js';
+import SideBar from './SideBar.js';
+
+const Main = () => {
+
+    return <div className='Main'>
+        <SideBar/>
+        <h1>Dev.prototype.nealOverton()</h1>
+    </div>
+
+}
+
+export default Main;
