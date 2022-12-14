f64ba9d79939f702bc6186c0b4bbd96bf7fdabe1
nealoverton <nealjoverton@gmail.com>
2022-12-14 22:26:35 +0000

    flesh out main page content

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,53 @@
+import { useState } from 'react';
 import '../styles/styles.js';
 
 const Main = () => {
-    const codeText = "const newDev = new Dev(2020);\n \t \n \t.then((data) => console.log(data));"
+    const nealOverton = <span className='red'>nealOverton</span>
+    const blockOpen = <span>&#62; &#123; &#47;</span>;
+    const blockClose = <span>&#47; &#125;</span>;
 
-    const outputText = "> Put some stuff here";
+    const introText = <p className='Main__intro'>
+        <span className='code-background'><b>Neal Overton</b></span> is a <b>full stack dev</b> and <b>bootcamp tutor</b>. He is prepared to write masses of code for the sake of a visual pun.
+    </p>
+
+    const codeDemoText = <p>
+        <span className='comment'>&#47;&#47;  create a new instance of Dev</span><br/>
+        <span className='blue'>const</span> newDev = new <span className='red'>Dev</span>();<br/><br/>
+        <span className='comment'>&#47;&#47;  call and store .nealOverton() with valid start date</span><br/>
+        <span className='blue'>const</span> data = newDev.{nealOverton}(<span className='green'>2019</span>);<br/><br/>
+        console.<span className='red'>log</span>(data);<br/>
+        <span className='comment'>&#47;&#47;  expected output: data specific to Neal Overton</span><br/>
+    </p>
+    
+    const outputText =  <p className='code-demo__output-row__output-text'>
+        &#62; I started teaching myself Java in 2019 using online resources. Yes, that was an intense place to start and yes, I could write an abstract class before I could make a simple static web page.<br/>
+        I loved it though and completing the Northcoders Coding Bootcamp in 2022 gave me a more rounded set of skills. Take a look below for more details.
+    </p>;
+   
+
+    const skillsText = <p className='skills-box__text'>
+        <span className='comment'>&#47;&#47;  Languages</span><br/>
+        {nealOverton}((<span className='blue'>JavaScriptES6</span>, <span className='blue'>Node.js</span>) ={blockOpen}* advanced, used throughout bootcamp and beyond *{blockClose})<br/>
+        {nealOverton}((<span className='blue'>Java</span>) ={blockOpen}* self-taught up to advanced concepts like interfaces and generics*{blockClose})<br/>
+        {nealOverton}((<span className='blue'>HTML</span>) ={blockOpen}* ... *{blockClose})<br/>
+        {nealOverton}((<span className='blue'>CSS</span>) ={blockOpen}* ... *{blockClose})<br/>
+        {nealOverton}((<span className='blue'>C#</span>) ={blockOpen}* experience is limited to experimenting with Unity, but my Java experience means everything seems fairly familiar *{blockClose})<br/>
+        {nealOverton}((<span className='blue'>Dart</span>) ={blockOpen}* experience is limited to experimenting with Flutter, but my Java experience means everything seems fairly familiar *{blockClose})<br/>
+        <br/><span className='comment'>&#47;&#47;  Frameworks</span><br/>
+        {nealOverton}((<span className='blue'>React</span>) ={blockOpen}* extensive experience, take a look at examples below (including this one) *{blockClose})<br/>
+        {nealOverton}((<span className='blue'>Flutter</span>) ={blockOpen}* experimented with some basic apps, love how the layout and styling is done right there as you place the widget *{blockClose})<br/>
+        <br/><span className='comment'>&#47;&#47;  Other</span><br/>
+        {nealOverton}((<span className='blue'>PostgreSQL</span>) ={blockOpen}* example below *{blockClose})<br/>
+        {nealOverton}((<span className='blue'>Unity</span>) ={blockOpen}* example below *{blockClose})<br/>
+    </p>;
+    
+
+    const [outputIsVisible, setOutputIsVisible] = useState(false);
 
 
     return <div className='Main'>
         <h1 className='Main__title'>Dev.prototype.nealOverton()</h1>
-        <p>Neal Overton is a full stack dev and bootcamp tutor. He likes coding.</p>
+        {introText}
         <h2 className='Main__subtitle'>Try me</h2>
         <div className='code-demo'>
             <p className='code-demo__title'>JavaScript Demo: Dev.nealOverton()</p>
@@ -21,25 +60,27 @@ const Main = () => {
                     <p>5</p>
                     <p>6</p>
                     <p>7</p>
+                    <p>8</p>
                 </div>
                 <div className='code-demo__body__code-field'>
-                    <p>
-                    <span className='blue'>const</span> newDev = new <span className='red'>Dev</span>(<span className='green'>2020</span>);
-                    </p>
+                    {codeDemoText}
                 </div>
             </div>
             <div className='code-demo__output-row'>
                 <div className='code-demo__output-row__button-column'>
-                    <button className='code-demo__output-row__button'>Run &#62;</button>
-                    <button className='code-demo__output-row__button'>Reset</button>
+                    <button className='code-demo__output-row__button' onClick={() => setOutputIsVisible(true)}>Run &#62;</button>
+                    <button className='code-demo__output-row__button' onClick={() => setOutputIsVisible(false)}>Reset</button>
                 </div>
                 <div className='code-demo__output-row__output-box'>
-                    <p className='code-demo__output-row__output-text'>{outputText}</p>
+                    {outputIsVisible ? outputText : <></>}
                 </div>
             </div>
         </div>
         <h2 className='Main__subtitle'>Skills</h2>
-        <div></div>
+        <div className='skills-box'>
+            {skillsText}
+        </div>
+        <h2 className='Main__subtitle'>Projects</h2>
     </div>
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,27 +1,38 @@
 import '../styles/styles.js';
+import { IoFlask } from 'react-icons/io5';
+// import { Tooltip } from 'react-tooltip';
+import Tippy from '@tippyjs/react';
 
 const SideBar = () => {
+
     return <div className='SideBar'>
         <h2 className='SideBar__title'>Related Topics</h2>
         <h3 className='SideBar__subtitle'>Skills</h3>
+        <Tippy content="hi">
+            <p><span>
+            <IoFlask className='blue' id="experimental" />
+            </span>Dev.prototype.C#()</p>
+        </Tippy> 
+        <p>Dev.prototype.CSS()</p>
+        <p><IoFlask className='blue'/>Dev.prototype.Dart()</p>
         <p>Dev.prototype.ES6/Node.js()</p>
-        <p>Dev.prototype.HTML5()</p>
-        <p>Dev.prototype.CSS3()</p>
-        <p>Dev.prototype.React()</p>
+        <p><IoFlask className='blue'/>Dev.prototype.Flutter()</p>
+        <p>Dev.prototype.HTML()</p>
         <p>Dev.prototype.Java()</p>
-        <p>Dev.prototype.C#()</p>
-        <p>Dev.prototype.Unity()</p>
         <p>Dev.prototype.PostgreSQL()</p>
+        <p>Dev.prototype.React()</p> 
+        <p>Dev.prototype.SQL()</p>
+        <p><IoFlask className='blue'/>Dev.prototype.Unity()</p>
 
         <h3 className='SideBar__subtitle'>Projects</h3>
-        <p>Dev.prototype.tablehog()</p>
-        <p>Dev.prototype.meetingRoomBooker()</p>
         <p>Dev.prototype.cavernOfWonder()</p>
-
+        <p>Dev.prototype.meetingRoomBooker()</p>
+        <p>Dev.prototype.tablehog()</p>
+        
         <h3 className='SideBar__subtitle'>Experience</h3>
-        <p>Dev.prototype.tutorManchesterCodes()</p>
         <p>Dev.prototype.northcodersCodingBootcamp()</p>
         <p>Dev.prototype.sportsTradingConsultancy()</p>
+        <p>Dev.prototype.tutorManchesterCodes()</p>
     </div>
 }
 
