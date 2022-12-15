82ab71d77c3a5fc0bed703b3193b5e43a1f41d83
nealoverton <nealjoverton@gmail.com>
2022-12-15 23:06:32 +0000

    show tooltips and start of responsiveness

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 import '../styles/styles.js';
 import { IoFlask } from 'react-icons/io5';
-// import { Tooltip } from 'react-tooltip';
-import Tippy from '@tippyjs/react';
+import { Tooltip } from 'react-tippy';
+import 'react-tippy/dist/tippy.css'
 import { HashLink as Link } from 'react-router-hash-link';
 
 const SideBar = () => {
@@ -10,21 +10,39 @@ const SideBar = () => {
         <h2 className='SideBar__title'>Related Topics</h2>
         <h3 className='SideBar__subtitle'>Skills</h3>
         
-        <Link smooth to={'#languages'} className='SideBar__link'><span>
-            <IoFlask className='blue' id="experimental" />
-            </span>Dev.prototype.C#()</Link>
+        <Link smooth to={'#languages'} className='SideBar__link'>
+            <span>
+                <Tooltip title="In experimental phase" position="top">
+                    <IoFlask className='blue' id="experimental" />
+                </Tooltip>
+            </span>Dev.prototype.C#()
+        </Link>
         <Link smooth to={'#languages'} className='SideBar__link'>Dev.prototype.CSS()</Link>
-        <Link smooth to={'#languages'} className='SideBar__link'><IoFlask className='blue'/>Dev.prototype.Dart()</Link>
-        
+        <Link smooth to={'#languages'} className='SideBar__link'>
+            <Tooltip title="In experimental phase" position="top">
+                <IoFlask className='blue' id="experimental" />
+            </Tooltip>
+            Dev.prototype.Dart()
+        </Link>
         <Link smooth to={'#languages'} className='SideBar__link'>Dev.prototype.ES6/Node.js()</Link>
-        <Link smooth to={'#frameworks'} className='SideBar__link'><IoFlask className='blue'/>Dev.prototype.Flutter()</Link>
+        <Link smooth to={'#frameworks'} className='SideBar__link'>
+            <Tooltip title="In experimental phase" position="top">
+                <IoFlask className='blue' id="experimental" />
+            </Tooltip>
+            Dev.prototype.Flutter()
+        </Link>
         <Link smooth to={'#languages'} className='SideBar__link'>Dev.prototype.HTML()</Link>
         <Link smooth to={'#languages'} className='SideBar__link'>Dev.prototype.Java()</Link>
         <Link smooth to={'#frameworks'} className='SideBar__link'>Dev.prototype.Jest()</Link>
         <Link smooth to={'#other'} className='SideBar__link'>Dev.prototype.PostgreSQL()</Link>
         <Link smooth to={'#frameworks'} className='SideBar__link'>Dev.prototype.React()</Link>
         <Link smooth to={'#languages'} className='SideBar__link'>Dev.prototype.SQL()</Link>
-        <Link smooth to={'#other'} className='SideBar__link'><IoFlask className='blue'/>Dev.prototype.Unity()</Link>
+        <Link smooth to={'#other'} className='SideBar__link'>
+            <Tooltip title="In experimental phase" position="top">
+                <IoFlask className='blue' id="experimental" />
+            </Tooltip>
+            Dev.prototype.Unity()
+        </Link>
 
         <h3 className='SideBar__subtitle'>Projects</h3>
         <Link smooth to={'#cavern'} className='SideBar__link'>Dev.prototype.cavernOfWonder()</Link>
