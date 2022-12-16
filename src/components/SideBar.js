a20a95378cf1eb55f84b92bcf555270cdd83ca57
nealoverton <nealjoverton@gmail.com>
2022-12-16 16:34:45 +0000

    sidebar lists are collapsable and expandable

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,26 @@
 import '../styles/styles.js';
-import { IoFlask } from 'react-icons/io5';
+import { IoFlask, IoCaretDown, IoCaretForward } from 'react-icons/io5';
 import { Tooltip } from 'react-tippy';
 import 'react-tippy/dist/tippy.css'
 import { HashLink as Link } from 'react-router-hash-link';
+import { useState } from 'react';
 
 const SideBar = () => {
+    const [skillListIsOpen, setSkillListIsOpen] = useState(true);
+    const [projectListIsOpen, setProjectListIsOpen] = useState(true);
+    const [experienceListIsOpen, setExperienceListIsOpen] = useState(true);
 
     return <div className='SideBar'>
         <h2 className='SideBar__title'>Related Topics</h2>
-        <h3 className='SideBar__subtitle'>Skills</h3>
+        <h3 className='SideBar__subtitle' onClick={()=>setSkillListIsOpen(!skillListIsOpen)}>
+            {skillListIsOpen ?
+            <IoCaretDown/> :
+            <IoCaretForward/>
+            }         
+            Skills
+        </h3>
         
+        <div className={skillListIsOpen ? 'SideBar__List' : 'SideBar__List--hidden'}>
         <Link smooth to={'#languages'} className='SideBar__link'>
             <span>
                 <Tooltip title="In experimental phase" position="top">
@@ -43,16 +54,35 @@ const SideBar = () => {
             </Tooltip>
             Dev.prototype.Unity()
         </Link>
-
-        <h3 className='SideBar__subtitle'>Projects</h3>
-        <Link smooth to={'#cavern'} className='SideBar__link'>Dev.prototype.cavernOfWonder()</Link>
-        <Link smooth to={'#meeting'} className='SideBar__link'>Dev.prototype.meetingRoomBooker()</Link>
-        <Link smooth to={'#tablehog'} className='SideBar__link'>Dev.prototype.tablehog()</Link>
+        </div>
         
-        <h3 className='SideBar__subtitle'>Experience</h3>
-        <Link smooth to={'#northcoders'} className='SideBar__link'>Dev.prototype.northcodersCodingBootcamp()</Link>
-        <Link smooth to={'#trading'} className='SideBar__link'>Dev.prototype.sportsTradingConsultancy()</Link>
-        <Link smooth to={'#mcrcodes'} className='SideBar__link'>Dev.prototype.tutorManchesterCodes()</Link>
+        <h3 className='SideBar__subtitle' onClick={()=>setProjectListIsOpen(!projectListIsOpen)}>
+            {projectListIsOpen ?
+                <IoCaretDown/> :
+                <IoCaretForward/>
+            }  
+            Projects
+        </h3>
+
+        <div className={projectListIsOpen ? 'SideBar__List' : 'SideBar__List--hidden'}>
+            <Link smooth to={'#cavern'} className='SideBar__link'>Dev.prototype.cavernOfWonder()</Link>
+            <Link smooth to={'#meeting'} className='SideBar__link'>Dev.prototype.meetingRoomBooker()</Link>
+            <Link smooth to={'#tablehog'} className='SideBar__link'>Dev.prototype.tablehog()</Link>
+        </div>
+        
+        <h3 className='SideBar__subtitle' onClick={()=>setExperienceListIsOpen(!experienceListIsOpen)}>
+            {experienceListIsOpen ?
+                <IoCaretDown/> :
+                <IoCaretForward/>
+            } 
+            Experience
+        </h3>
+        <div className={experienceListIsOpen ? 'SideBar__List' : 'SideBar__List--hidden'}>
+            <Link smooth to={'#northcoders'} className='SideBar__link'>Dev.prototype.northcodersCodingBootcamp()</Link>
+            <Link smooth to={'#trading'} className='SideBar__link'>Dev.prototype.sportsTradingConsultancy()</Link>
+            <Link smooth to={'#mcrcodes'} className='SideBar__link'>Dev.prototype.tutorManchesterCodes()</Link>
+        </div>
+        
     </div>
 }
 
